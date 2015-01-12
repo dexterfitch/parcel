97b959e8ac1d8e1356ab15e9e932c264fc4d68e4
Dexter Fitch <fitch.dexter@gmail.com>
2015-01-12 14:25:39 -0800

    add working web functionality with Sinatra

---
+++ app.rb
@@ -0,0 +1,18 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/parcel.rb')
+
+get('/') do
+  erb(:form)
+end
+
+get('/parcel') do
+  @length = params.fetch('length').to_f
+  @width = params.fetch('width').to_f
+  @height = params.fetch('height').to_f
+  @weight = params.fetch('weight').to_f
+  instance = Parcel.new(@length, @width, @height, @weight)
+  @result = instance.cost_to_ship()
+  erb(:parcel)
+end
